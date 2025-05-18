fcf660236e5271ac183027cdf6a7d01f822883ba
Myroslav <verstyukm@gmail.com>
2025-05-18 03:14:42 +0300

    Add email module

---
+++ src/email/email.module.ts
@@ -0,0 +1,40 @@
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as path from 'path';
+import { EmailService } from './email.service';
+
+@Module({
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get<string>('SMTP_HOST'),
+          port: config.get<number>('SMTP_PORT'),
+          secure: false,
+          auth: {
+            user: config.get<string>('SMTP_USER'),
+            pass: config.get<string>('SMTP_PASSWORD'),
+          },
+        },
+        defaults: {
+          from: config.get<string>('EMAIL_FROM'),
+        },
+        template: {
+          dir: path.resolve(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}
---
+++ src/email/email.service.ts
@@ -0,0 +1,51 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EmailService {
+  private readonly appUrl: string;
+
+  constructor(
+    private readonly mailer: MailerService,
+    private readonly config: ConfigService,
+  ) {
+    this.appUrl = `${this.config.get<string>('HOST')}:${this.config.get<string>('PORT')}`;
+  }
+
+  async sendConfirmationEmail(email: string, token: string) {
+    const url = `${this.appUrl}/api/confirm/${token}`;
+    await this.mailer.sendMail({
+      to: email,
+      subject: 'Please confirm your Weather API subscription',
+      template: './confirmation',
+      context: {
+        url,
+      },
+    });
+  }
+
+  async sendWeatherUpdateEmail(
+    email: string,
+    city: string,
+    temperature: number,
+    humidity: number,
+    description: string,
+    token: string,
+  ) {
+    const unsubscribeUrl = `${this.appUrl}/api/unsubscribe/${token}`;
+    await this.mailer.sendMail({
+      to: email,
+      subject: `Weather update for ${city}`,
+      template: './weather-update',
+      context: {
+        city,
+        temperature,
+        humidity,
+        description,
+        timestamp: new Date().toLocaleString(),
+        unsubscribeUrl,
+      },
+    });
+  }
+}
